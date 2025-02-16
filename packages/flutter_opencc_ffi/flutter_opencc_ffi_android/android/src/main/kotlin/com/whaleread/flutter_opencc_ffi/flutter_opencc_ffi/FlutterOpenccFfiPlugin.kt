8843fe495faf8684398fe499be68271eb64bf08a
oldcookie <jxky2004@gmail.com>
2025-02-16 17:26:58 +0800

    新增 flutter_opencc_ffi 子專案，包含 Android 和 iOS 配置，更新相關資源檔案與設定

---
+++ packages/flutter_opencc_ffi/flutter_opencc_ffi_android/android/src/main/kotlin/com/whaleread/flutter_opencc_ffi/flutter_opencc_ffi/FlutterOpenccFfiPlugin.kt
@@ -0,0 +1,15 @@
+package com.whaleread.flutter_opencc_ffi.flutter_opencc_ffi
+
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+/** FlutterOpenccPlugin */
+class FlutterOpenccFfiPlugin: FlutterPlugin {
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+  }
+}
