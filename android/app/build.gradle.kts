4857615c1347a21f089da5f0f2f7bef67b34d24e
oldcookie <jxky2004@gmail.com>
2025-02-26 22:06:01 +0800

    更新 coreLibraryDesugaring 版本至 2.1.5

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.2.2")
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.5")
     implementation("androidx.multidex:multidex:2.0.1")
 }
 
