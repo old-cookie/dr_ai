243bb58e6703134ce5e7ee1c05bcafd348e78a15
oldcookie <jxky2004@gmail.com>
2025-03-27 23:03:32 +0800

    更新版本號至5.0.0+1，移除未使用的導入

--- lib/services/service_notification.dart
+++ lib/services/service_notification.dart
@@ -3,7 +3,6 @@ import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:flutter_timezone/flutter_timezone.dart';
 import 'package:flutter/material.dart';
-import 'dart:typed_data';
 
 class NotificationService {
   static final NotificationService _instance = NotificationService._();
